8565d0020bfa6285d6ef75646f8c99317d68c926
lalitkumar88 <117524780+lalitkumar88@users.noreply.github.com>
2025-04-04 07:56:48 +0000

    basics routers exercize done

---
+++ react-router/src/App.jsx
@@ -0,0 +1,254 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+const Home = () => <h1>Home page</h1>;
+const AboutUs = () => <h1>About Us page</h1>;
+const ContactUs = () => <h1>Contact Us page</h1>;
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="con-wrap">
+          <header>this is test line</header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />}></Route>
+          </Routes>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            dolores aspernatur molestias mollitia, rerum eaque magnam provident
+            aliquam iusto vel veniam eos, magni nemo a nesciunt laudantium
+            ratione hic, aliquid qui. Harum maiores aperiam minus, corporis
+            delectus sit consequatur soluta quos ex quia quae neque eos maxime?
+            Commodi, provident officiis?
+          </p>
+        </div>
+        <footer>
+          <p>made by lalitkumar88</p>
+          <p>2025</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
